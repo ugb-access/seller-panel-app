f0a0f17b5c5274522668aaed3b5db1cd21231bb0
rizwan <rizwanrj4u@gmail.com>
2020-10-22 12:42:31 +0500

    latest pull

--- src/components/Headers/PrimaryHeader.jsx
+++ src/components/Headers/PrimaryHeader.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { ReactComponent as Logo } from '../../images/logos/logo.svg';
 import '../../scss/headers.scss';
+import '../../scss/containers.scss';
 
 class PrimaryHeader extends Component {
     render() {
         return(
-            <div className="header-container bg-white primary-header">
+            <div className="header-container bg-white primary-header container">
                 <div className="header-inner-wrapper">
                     <div className="header-logo-wrapper">
                         <Logo />
